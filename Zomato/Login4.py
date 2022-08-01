e22016baef10291473315fc20dd9d2e0674e819a
Manoj0705 <108943892+Manoj0705@users.noreply.github.com>
2022-08-01 12:31:40 +0530

    Update Login4.py

--- Zomato/Login4.py
+++ Zomato/Login4.py
@@ -1,11 +1,11 @@
-print("login to your account")
-a=input("Enter the user ID:\n")
-b=input("Enter the password\n")
+prit("login to your account")
+a=inpu("Enter the user ID:\n")
+b=inpu("Enter the password\n")
 c="admin"
 d="admin"
 e="logined successfully"
 f="User ID and Password does not match"
-if a==c and b==d:
+if a=c and b=d:
     print(e)
 else:
     print(f)
