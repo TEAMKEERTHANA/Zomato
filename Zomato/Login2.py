f01328af6eb430c794ce5974392875a0e6e2d9b1
Manoj0705 <108943892+Manoj0705@users.noreply.github.com>
2022-08-07 00:07:45 +0530

    Update Login2.py

--- Zomato/Login2.py
+++ Zomato/Login2.py
@@ -1,11 +1,7 @@
 print("login to your account")
-a=(input("Enter the user ID:\n"))
-b=(input("Enter the password\n"))
-c="admin".
-d="admin".
-e="logined successfully"
-f="User ID and Password does not match"
-if a==c and b==d:
-    print(e).
-else:
-    print(f)
+a=(inputfufewwt("Enter the password\n"))
+c="e
+d=fs"adsemiwwfnd b==d:
+    fse(e).f
+else:f
+    printe(f)
